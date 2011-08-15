c9e74238f5d79c889c82f25a27fcf8069ddda881
Arild Wollf <arild@numericalrocks.com>
2011-08-15 10:44:57 +0200

    Indented to two spaces, to test committing to github.

--- test.cpp
+++ test.cpp
@@ -9,39 +9,39 @@
 #include <QtGui/QBoxLayout>
 #include <QtGui/QPushButton>
 
- int main(int argc, char *argv[])
- {
-     QApplication app(argc, argv);
-     app.setOrganizationName("Numerical Rocks");
-     app.setApplicationName("Application Example");
-     QStyle * style;
-     style = new QCleanlooksStyle();
-     //style = new QProxyStyle();
-     app.setStyle(style);
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  app.setOrganizationName("Numerical Rocks");
+  app.setApplicationName("Application Example");
+  QStyle * style;
+  style = new QCleanlooksStyle();
+  //style = new QProxyStyle();
+  app.setStyle(style);
 
-     QMainWindow mainWin;
-     mainWin.setContentsMargins(0,0,0,0);
+  QMainWindow mainWin;
+  mainWin.setContentsMargins(0,0,0,0);
 
-     QWidget * w = new QWidget(&mainWin);
-     mainWin.setCentralWidget(w);
+  QWidget * w = new QWidget(&mainWin);
+  mainWin.setCentralWidget(w);
 
 
-     QLayout * layout = new QHBoxLayout(w);
-     w->setLayout(layout);
+  QLayout * layout = new QHBoxLayout(w);
+  w->setLayout(layout);
 
 #if 0
-     QSlider * slider;
-     slider = new QSlider(Qt::Horizontal,w);
+  QSlider * slider;
+  slider = new QSlider(Qt::Horizontal,w);
 #else
-     QRangeSlider * slider;
-     slider = new QRangeSlider(Qt::Horizontal,w);
-     slider->setMaxRange(QPair<int,int>(50,200));
+  QRangeSlider * slider;
+  slider = new QRangeSlider(Qt::Horizontal,w);
+  slider->setMaxRange(QPair<int,int>(50,200));
 #endif
-     //slider->setTickPosition(QSlider::TicksBelow);
-     slider->setSizePolicy(QSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding));
-     layout->addWidget(slider);
+  //slider->setTickPosition(QSlider::TicksBelow);
+  slider->setSizePolicy(QSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding));
+  layout->addWidget(slider);
 
-     mainWin.show();
-     return app.exec();
+  mainWin.show();
+  return app.exec();
 
- }
+}
